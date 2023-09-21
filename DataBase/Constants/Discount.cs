315eb709767727943d89f7b2451b1a481ddbb675
veronika-ignatova <veronika.a.ignatova@gmail.com>
2023-09-21 18:04:42 +0300

    new discount

--- DataBase/Constants/Discount.cs
+++ DataBase/Constants/Discount.cs
@@ -11,18 +11,10 @@ namespace DataBase.Constants
     internal partial class MyDbContext
     {
         public static IEnumerable<Discount> Discounts = new List<Discount>
-        {
+        {          
             new Discount()
             {
                 Id = 1,
-                DiscountType = DiscountType.None,
-                Name = "Discount1",
-                Description = "Discount1 with 10%",
-                Value = 0
-            },            
-            new Discount()
-            {
-                Id = 2,
                 DiscountType = DiscountType.Birthday,
                 Name = "Discount2",
                 Description = "Discount2 with 20%",
@@ -30,7 +22,7 @@ namespace DataBase.Constants
             },            
             new Discount()
             {
-                Id = 3,
+                Id = 2,
                 DiscountType = DiscountType.Holiday,
                 Name = "Discount3",
                 Description = "Discount3 with 30%",
@@ -38,7 +30,7 @@ namespace DataBase.Constants
             },            
             new Discount()
             {
-                Id = 4,
+                Id = 3,
                 DiscountType = DiscountType.Holiday,
                 Name = "Discount4",
                 Description = "Discount4 with 40%",
@@ -46,7 +38,7 @@ namespace DataBase.Constants
             },            
             new Discount()
             {
-                Id = 5,
+                Id = 4,
                 DiscountType = DiscountType.Holiday,
                 Name = "Discount5",
                 Description = "Discount5 with 50%",
